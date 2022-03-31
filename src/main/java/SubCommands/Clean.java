d13e6298cbfded582706b50e5840575f08505044
Dimitri <dimitri.debleser@heig-vd.ch>
2022-03-31 19:32:33 +0200

    fix-clear-real-path

--- src/main/java/SubCommands/Clean.java
+++ src/main/java/SubCommands/Clean.java
@@ -2,14 +2,20 @@ package SubCommands;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "clean", description = "Clean a static site")
 public class Clean implements Callable<Integer> {
 
+    @CommandLine.Parameters(paramLabel = "SITE", description = "The site to build")
+    public Path site;
+
     @Override public Integer call() throws IOException {
-        File dir = new File("./mon");
+        File dir = site.toFile();
         if (!dir.exists()) {
             System.out.println("Nothing to clean");
             return 0;
