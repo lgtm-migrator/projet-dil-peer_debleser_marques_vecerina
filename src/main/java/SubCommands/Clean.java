5d00673822f3e333df9c3b64d5a0580ac322d223
VincentPeer <vincent.peer@heig-vd.ch>
2022-03-31 11:43:59 +0200

    take src from teacher's repo

---
+++ src/main/java/SubCommands/Clean.java
@@ -0,0 +1,14 @@
+package SubCommands;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import picocli.CommandLine.Command;
+
+@Command(name = "clean", description = "Clean a static site")
+public class Clean implements Callable<Integer> {
+
+    @Override public Integer call() throws IOException {
+        return 0;
+    }
+
+}
