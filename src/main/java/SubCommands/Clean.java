a1a4e464804d1d54a6f002307a519a3e8a983f91
Dimitri <dimitri.debleser@heig-vd.ch>
2022-03-31 15:50:07 +0200

    implementing clean methods

--- src/main/java/SubCommands/Clean.java
+++ src/main/java/SubCommands/Clean.java
@@ -1,5 +1,6 @@
 package SubCommands;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
@@ -8,7 +9,30 @@ import picocli.CommandLine.Command;
 public class Clean implements Callable<Integer> {
 
     @Override public Integer call() throws IOException {
+        File dir = new File("./mon");
+        if (!dir.exists()) {
+            System.out.println("Nothing to clean");
+            return 0;
+        }
+        deleteFile(dir);
+        System.out.println("Website files cleaned");
         return 0;
+    }
+
+    private void deleteFile(File file) {
+        if (file.exists()) {
+            // Check if directory
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null)
+                    // If is dir and has files delete these first
+                    for (File f : files)
+                        deleteFile(f);
+            }
+
+            // Finally delete file / folder
+            file.delete();
+        }
     }
 
 }
